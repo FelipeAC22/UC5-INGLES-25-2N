a90109d3fc012a127e835abc6e9e27f484ccdeff
FELIPE ANTUNES CARDOSO <04471805096@senacrs.edu.br>
2026-03-06 21:32:18 -0300

    adicionei o código melhorado do jogo de detetive

--- DEV-LINGO/src/games/detectiveStory.js
+++ DEV-LINGO/src/games/detectiveStory.js
@@ -1,27 +1,62 @@
 import * as ask from 'readline-sync';
-import { addScore, loseLife} from "../gameState.js";
+import { addScore, loseLife } from "../gameState.js";
 
 export function detectiveStory() {
 
     console.log("\nYou are a detective solving a mystery.")
+    ask.question("Press ENTER to start the investigation...")
+    console.clear()
 
-    console.log("\nChoose the correct word:")
-    console.log("The suspect ____ nervous.")
+    let Story = [
+        {
+            scene: "You ____ the crime scene",
+            options: ["investigate", "investigates", "insvestigating"],
+            correct: "investigate"
+        },
+        {
+            scene: "you start ____ the peoples in the local",
+            options: ["interrogates", "interrogating", "interrogate"],
+            correct: "interrogating"
+        },
+        {
+            scene: "After collect informations you reach at the _____ suspect",
+            options: ["principal", "principals", "principally"],
+            correct: "principal"
+        },
+        {
+            scene: "You push the suspect to ____ the truth",
+            options: ["speaks", "speaking", "speak"],
+            correct: "speak"
+        },
+        {
+            scene: "The suspect ____ nervous ",
+            options: ["look", "looks", "looking"],
+            correct: "looks"
+        },
+        {
+            scene: "After the interrogatory, the suspect ____ his guilt",
+            options: ["confesses", "confess", "confessing"],
+            correct: "confesses"
+        }
+    ]
 
-    console.log("1) look")
-    console.log("2) looks")
-    console.log("3) looking")
+    Story.forEach(s => {
+        console.log("\nChoose the correct word to solve this clue:\n")
+        console.log(s.scene)
+        console.log(`\nWords: ${s.options.join(" | ")}`)
 
-    let answer = ask.question("Option: ")
+        let answer = ask.question("\nOption: ")
 
-    if (answer === "2") {
-        console.log("Clue discovered!")
-        addScore(15)
-    } else {
-        console.log("Wrong clue!")
-        loseLife()
-    }
+        if (answer.toLocaleLowerCase() === s.correct) {
+            console.log("\nClue discovered!")
+            addScore(15)
+        } else {
+            console.log("\nWrong clue!")
+            loseLife()
+        }
 
-    ask.question("\nPress ENTER to continue...")
+        ask.question("\nPress ENTER to next clue...")
+        console.clear()
+    })
+    ask.question("Congrats you solve the mistery! Press ENTER to continue...")
 }
-
